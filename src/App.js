50608429f954b1bea4a066c9466347d9343eb67c
lanachiad <lanachiad7@gmail.com>
2018-04-30 15:26:03 -0400

    Added more info

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Depending on what the weather is, this page will render a simulated weather experience.</h1>
-        <h2>Until I have this figured out, I'll simulate them through buttons, I guess</h2> 
+        <h2>Until I have this figured out, I'll simulate them through buttons, I guess</h2>
+        <h3>Oh and this will only be for NYC, so sorry 'bout that</h3> 
       </div>
     );
   }
