bb6b502c49b04457729d577f48b0d22777be3bd6
lanachiad <lanachiad7@gmail.com>
2018-04-30 15:19:50 -0400

    Stripped default content and styling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Depending on what the weather is, this page will render a simulated weather experience.</h1>
+        <h2>Until I have this figured out, I'll simulate them through buttons, I guess</h2> 
       </div>
     );
   }
