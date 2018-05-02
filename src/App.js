c84d971959978746b970ac133c674e1a52afb1b7
lanachiad <lanachiad7@gmail.com>
2018-05-02 13:40:24 -0400

    Refactor so that the splash screen is a component that can be switched off

--- src/App.js
+++ src/App.js
@@ -1,32 +1,13 @@
 import React, { Component } from 'react';
-import Rain from './Rain';
+import ShowScreen from './ShowScreen';
 
 class App extends Component {
-  constructor(props) {
-  	super(props);
-  	this.state = {
-  		showRain: false
-  	}
-  }
-
-  _onRainBtnClick = () => {
-  	let currentRainState = this.state.showRain;
-  	this.setState({ showRain: !currentRainState })
-  }
+  
 
   render() {
     return (
       <div className="App">
-        <h1>Depending on what the weather is, this page will render a simulated weather experience.</h1>
-        <h2>Until I have this figured out, I'll simulate them through buttons, I guess</h2>
-        <h3>Oh and this will only be for NYC, so sorry 'bout that</h3> 
-        <button onClick={this._onRainBtnClick}>Rain</button>
-        <div className="weather-section">
-        	{this.state.showRain ? 
-        		<Rain /> : 
-        		<p>No weather to show</p>
-        	}
-        </div>
+        <ShowScreen />
       </div>
     );
   }
