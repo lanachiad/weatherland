5bb2a80b29a09eed73528fb8ca2831c39ebcafec
lanachiad <lanachiad7@gmail.com>
2018-05-02 12:45:41 -0400

    Add test content inside div to render

--- src/Rain.js
+++ src/Rain.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import './Rain.css';
 
 class Rain extends Component {
 	render() {
 		return (
 			<div className="rainy-day">
+				<p>It's raining</p>
 			</div>
 		)
 	}
